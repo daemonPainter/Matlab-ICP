82a4b39947f957e2cc846037475ad8453f875ad1
daemonPainter <mail@gabrieleomodeo.it>
2015-12-13 11:34:10 +0100

    major update
    - symbols have been changed so they match ones from reference literature
    - new comments

--- closest_points.m
+++ closest_points.m
@@ -1,22 +1,33 @@
-function [ cp ] = closest_points( source, model )
+function [ Y ] = closest_points( P, X )
 % Find closest points between two sets of points.
-%   Input must be 4xN. Will reorder points of source so they match the same
-%   index of the closest neighbour in model.
+%   P, X must be 4-by-n in homogeneous coordinates or 3-by-n in cartesian
+%   coordinates.
+%   [Y] = closest_points(P,X) will reorder points of P so they match the
+%   same order of the relative closest neighbour in X and will save the
+%   re-ordered P set in Y.
+%   Works only if X and P have the same number of points. See issue #3 on
+%   repository if you'd like to contribute.
 
-source = source(1:3,:);
-model = model(1:3,:);
+P = P(1:3,:);
+X = X(1:3,:);
 
-n_points = size(source,2);
+n_points = size(P,2);
+Y = zeros(3,n_points);
 
-source_mean = mean(source,2);
+% mean value to remove errors and disturbances
+P_mean = mean(P,2);
+X_mean = mean(X,2);
 
-source = source - repmat(source_mean, 1, n_points);
-model = model - repmat(mean(model, 2), 1, n_points);
+P = P - repmat(P_mean, 1, n_points);
+X = X - repmat(X_mean, 1, n_points);
 
 for i = 1:n_points
-    diff_matrix = (repmat(model(:,i), 1, n_points) - source);
+    % create a matrix the same size of P with only one point of X,
+    diff_matrix = (repmat(X(:,i), 1, n_points) - P);
+    % find distances between all these points,
     distance = sqrt(sum(diff_matrix.^2, 1));
+    % we are interested in the closest one,
     [~, index] = min(distance);
-    cp(:, i) = source(:,index) + source_mean;
-
+    % select that points (add mean value)
+    Y(:, i) = P(:,index) + P_mean;
 end
